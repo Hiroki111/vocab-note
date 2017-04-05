a740b2dc0fc48368e0677187107788e8882c274e
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2017-04-06 07:19:11 +1000

    Fixed the Modal in App.js

--- resources/assets/js/components/App.js
+++ resources/assets/js/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Note from './Note';
-import WordModal from './WordModal';
+import { Modal } from 'react-bootstrap';
 import {connect} from 'react-redux';
 import * as wordAction from '../actions/wordActions';
+import WordForm from './WordForm';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
     };
     this.handleHideModal = this.handleHideModal.bind(this);
     this.handleShowModal = this.handleShowModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleHideModal(){
@@ -27,16 +29,41 @@ class App extends React.Component {
     }));
     this.setState({showModal: true});
   }
+  handleSubmit(values){
+   fetch('/api/words/', {
+     method: 'POST',
+     headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify({
+       word: values.word,
+       pronunciation: values.pronunciation,
+       type:values.type,
+       meaning:values.meaning,
+       example:values.example,
+     }),
+   }).then((response)=>{
+     response.json().then((jsonReponse) => {
+       this.props.dispatch(wordAction.addWord(jsonReponse));
+     });
+     this.handleHideModal();
+   });
+ }
 
   render() {
     return (
       <div className="flex-center position-ref full-height">
         <div className="top-right links">
-          <button className="" onClick={this.handleShowModal}>Register new word</button>
-            {this.state.showModal ? <WordModal
-             handleHideModal={this.handleHideModal}
-             title={"New Word"}
-             word={this.state.word} /> : null}
+          <a href="#new_Word" onClick={this.handleShowModal}>Register New Word</a>
+          <Modal show={this.state.showModal} onHide={this.handleHideModal}>
+            <Modal.Header>
+              <Modal.Title>New Word</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <WordForm onSubmit={this.handleSubmit} />
+            </Modal.Body>
+          </Modal>
         </div>
         <div className="content">
           <div className="title">Hiroki's Vocab Note</div>
