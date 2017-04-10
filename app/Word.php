fd917abd488ecd8e55547bc5fb1f6ed0f1744bcd
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2017-04-11 07:21:58 +1000

    fixed untracked files, and added soft_delete to words table

--- app/Word.php
+++ app/Word.php
@@ -3,12 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Word extends Model
 {
     protected $table = 'words';
+    use SoftDeletes;
 
     protected $fillable = [
         'word', 'pronunciation', 'type', 'meaning', 'example',
     ];
+    protected $dates = ['deleted_at'];
 }
